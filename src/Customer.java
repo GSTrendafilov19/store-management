625a48a834403c752a6e65f96011dbbf8d8255f4
GSTrendafilov19 <GSTrendafilov19@codingburgas.bg>
2023-11-28 13:42:08 +0200

    Add Classes

---
+++ src/Customer.java
@@ -0,0 +1,41 @@
+public class Customer {
+    private String name;
+    private String address;
+    private int PIN;
+
+    public Customer() {
+        this.name = "";
+        this.address = "";
+        this.PIN = 0;
+    }
+
+    public Customer(String name, String address, int PIN) {
+        this.name = name;
+        this.address = address;
+        this.PIN = PIN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPIN() {
+        return PIN;
+    }
+
+    public void setPIN(int PIN) {
+        this.PIN = PIN;
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,57 @@
+public class Product {
+    private int ID;
+    private String brand;
+    private String name;
+    private float price;
+
+    public Product() {
+        this.ID = 0;
+        this.brand = "";
+        this.name = "";
+        this.price = 0;
+    }
+
+    public Product(int ID, String brand, String name, float price) {
+        this.ID = ID;
+        this.brand = brand;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + this.ID + "Brand: " + this.brand + "Name: " + this.name + "Price: " + this.price;
+    }
+}
